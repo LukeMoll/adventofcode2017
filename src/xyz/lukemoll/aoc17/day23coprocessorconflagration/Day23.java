4b977b39823d96734849240818957d9cf7e550b7
Luke Moll <luke.moll99@gmail.com>
2017-12-23 18:37:19 +0000

    Added solution for Day 23 part 2

--- src/xyz/lukemoll/aoc17/day23coprocessorconflagration/Day23.java
+++ src/xyz/lukemoll/aoc17/day23coprocessorconflagration/Day23.java
@@ -2,23 +2,15 @@ package xyz.lukemoll.aoc17.day23coprocessorconflagration;
 
 import java.util.Arrays;
 import java.util.function.Consumer;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import xyz.lukemoll.aoc17.day18duet.Day18;
+import java.util.regex.*;
 import xyz.lukemoll.aoc17.day18duet.Day18.AbstractMachine;
 import xyz.lukemoll.aoc17.utils.PuzzleInputReader;
 
 public class Day23 {
 
-	static class Coprocessor extends Day18.AbstractMachine {
+	static class Coprocessor extends AbstractMachine {
 
 		public int mulCount = 0;
-		
-		public Coprocessor(Consumer<AbstractMachine>[] program) {
-			super(program);
-		}
-		
 		public Coprocessor(Consumer<AbstractMachine>[] program, boolean debugMode) {
 			super(program);
 			if(!debugMode) {
@@ -29,25 +21,14 @@ public class Day23 {
 		public void snd(long value) {
 			this.mulCount++;
 		}
-		// Don't need this
-		public void rcv(String op1) {
-//			dump();
-		}
 		
-		public void dump() {
-			System.out.println("-");
-			for(String s : new String[]{"a","b","c","d","e","f","g","h"}) {
-				System.out.println(" " + s + ": " + this.getRegisterOrValue(s));
-			}
-		}
+		public void rcv(String op1) {}// Don't need this
 	}
 	
 	static Pattern INSTRUCTION_SET = Pattern.compile("^set\\s([a-z]+)\\s(-?[a-z0-9]+)$");
 	static Pattern INSTRUCTION_SUB = Pattern.compile("^sub\\s([a-z]+)\\s(-?[a-z0-9]+)$");
 	static Pattern INSTRUCTION_MUL = Pattern.compile("^mul\\s([a-z]+)\\s(-?[a-z0-9]+)$");
 	static Pattern INSTRUCTION_JNZ = Pattern.compile("^jnz\\s(-?[a-z0-9]+)\\s(-?[a-z0-9]+)$");
-	
-	
 	public static Consumer<AbstractMachine> decode(String instruction) {
 		Matcher match = INSTRUCTION_SET.matcher(instruction);
 		if(match.matches()) {
@@ -72,7 +53,6 @@ public class Day23 {
 			String 	op1 = match.group(1),
 					op2 = match.group(2);
 			return c -> {
-				c.rcv("");
 				if(c.getRegisterOrValue(op1) != 0) {
 						int jumpAmount = (int) c.getRegisterOrValue(op2);
 						if(jumpAmount != 0)
@@ -85,13 +65,52 @@ public class Day23 {
 		}
 	}
 	
+	static int getNumbersWithFactors(int bstart, int c, int difference) {
+		int h =0;
+		for(int b=bstart; b <= c; b += difference)  {
+			h += hasFactors(b);
+		}
+		return h;
+	}
+	
+	static int hasFactors(int b) {
+		for(int d=2; d!=b; d++) {
+	        for(int e=2; e<=(b/d); e++) {
+	            if(d*e == b) {
+	                return 1;// Normally f would be set to 0 and h would be incremented consequently.
+	            }
+	        }
+	    }
+		return 0;
+	}
+	
 	public static void main(String[] args) {
-		Consumer<AbstractMachine>[] program = Arrays.stream(PuzzleInputReader.getPuzzleInputLines("day23.txt"))
+		String[] input = PuzzleInputReader.getPuzzleInputLines("day23.txt");
+		
+		// Part 1
+		@SuppressWarnings("unchecked") // Can't do n -> new Consumer<AbstractMachine>[n]
+		Consumer<AbstractMachine>[] program = Arrays.stream(input)
 				.map(Day23::decode).toArray(n -> new Consumer[n]);
 		Coprocessor pt1 = new Coprocessor(program, true);
 		pt1.run();
-		System.out.println(pt1.mulCount);
-
+		System.out.println("Part 1: " + pt1.mulCount);
+		
+		// Part 2
+		if(program.length == 32) {
+			Consumer<AbstractMachine>[] header = Arrays.copyOf(program, 8);
+			Coprocessor pt2 = new Coprocessor(header, false);
+			pt2.run();
+			int difference = 17;
+			if(input[30].startsWith("sub b -")) {
+				difference = Integer.parseInt(input[30].substring(7));
+			}
+			int h = getNumbersWithFactors(	pt2.registers.get("b").intValue(),
+											pt2.registers.get("c").intValue(),
+											difference);
+			System.out.println(h + " numbers with factors");
+		}
+		else {
+			System.out.println("Unexpected program length; problem may have changed");
+		}
 	}
-
 }
